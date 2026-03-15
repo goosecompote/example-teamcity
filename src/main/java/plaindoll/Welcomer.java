332cd0a86ed8dae3873850c7a42f577d79ed5e81
Dmitriy <d.i.pavlov@yandex.ru>
2026-03-15 20:36:26 +0300

    fix

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,18 +1,16 @@
 package plaindoll;
 
-public class Welcomer{
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
+public class Welcomer {
+
+    public String sayWelcome() {
+        return "Hello";
+    }
+
+    public String sayFarewell() {
+        return "Bye";
+    }
+
+    public String reply() {
+        return "hunter says hello";
+    }
 }
